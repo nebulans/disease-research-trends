8c6dbf6dc29d68422e2b30da8cf39756e75a3249
Tim Davies <tim.m.davies@gmail.com>
2015-08-14 22:23:02 +0100

    Add bower support, materialize CSS framework

--- diseasemap.py
+++ diseasemap.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+from flask.ext.bower import Bower
+Bower(app)
 
 @app.route('/')
 def index():
-    return "Hello Flask"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
